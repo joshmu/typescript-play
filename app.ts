5539840c5cc08acaaa83fc0411d02c3ad4158b9b
Josh Mu <mu@joshmu.com>
2020-06-18 09:42:04 +1000

    objs-arrays-enums

--- app.ts
+++ app.ts
@@ -3,20 +3,33 @@
 //   name: string
 //   age: number
 // } = {
-const person: {
-  name: string
-  age: number
-  hobbies: string[]
-  role: [number, string]
-} = {
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   name: 'Josh',
+//   age: 34,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'], // array has to have exactly 2 elements
+// }
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: 'Josh',
-  age: 34,
+  age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'], // array has to have exactly 2 elements
+  role: Role.ADMIN,
 }
 
 // these could work if not using a 'Tuple'
-person.role.push('admin')
+// person.role.push('admin')
 // person.role[1] = 10
 
 let favouriteActivities: string[]
@@ -27,3 +40,5 @@ console.log(person.name)
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
 }
+
+if (person.role === Role.ADMIN) console.log('is admin')
