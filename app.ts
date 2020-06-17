1de88835f3df295b8a33b83c660aebdab97e4b05
Josh Mu <mu@joshmu.com>
2020-06-17 18:30:45 +1000

    other types

--- app.ts
+++ app.ts
@@ -3,9 +3,27 @@
 //   name: string
 //   age: number
 // } = {
-const person = {
-  name: 'josh',
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string]
+} = {
+  name: 'Josh',
   age: 34,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'], // array has to have exactly 2 elements
 }
 
+// these could work if not using a 'Tuple'
+person.role.push('admin')
+// person.role[1] = 10
+
+let favouriteActivities: string[]
+favouriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
