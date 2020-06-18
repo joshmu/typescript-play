73508479213adb85be18e1c548ddc112e38d0b84
Josh Mu <mu@joshmu.com>
2020-06-18 16:44:49 +1000

    source map to bridge ts in dev console

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,13 @@
 const username = 'josh'
 
 console.log(username)
+
+const button = document.querySelector('button')!
+
+function clickHandler(message: string) {
+  console.log('Clicked!', message)
+}
+
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, 'from josh'))
+}
