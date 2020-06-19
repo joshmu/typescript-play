b797be1631f245fc7e531367de6dc8f1b8fee4e9
Josh Mu <mu@joshmu.com>
2020-06-19 15:30:35 +1000

    readonly

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ class Department {
   // private employees: string[]
   private employees: string[] = []
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     // this.name = n
     // this.employees = []
   }
