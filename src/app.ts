c84774f3c4c905aabe4bff65646b11d7f0e635a0
Josh Mu <mu@joshmu.com>
2020-06-19 15:16:13 +1000

    private & public

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 class Department {
   name: string
+  private employees: string[]
 
   constructor(n: string) {
     this.name = n
+    this.employees = []
   }
 
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addemployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const accounting = new Department('Accounting')
-accounting.describe()
 
-const accountingCopy = { name: 'poo', describe: accounting.describe }
-accountingCopy.describe()
+accounting.addemployee('Josh')
+accounting.addemployee('Ben')
+// accounting.employees[2] = 'Anna' // <- this will not work due to 'private' prop
+
+accounting.describe()
+accounting.printEmployeeInformation()
