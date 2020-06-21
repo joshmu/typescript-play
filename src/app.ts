4225fca0e5436b41ddd0bbc56d4129de0273d0e0
Josh Mu <mu@joshmu.com>
2020-06-21 12:43:49 +1000

    type guards

--- src/app.ts
+++ src/app.ts
@@ -41,3 +41,55 @@ type Universal = Combinable & Numeric
 
 // we get the common types -> 'number'
 const u1: Universal = 999
+
+// type guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString()
+  }
+  return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+const em2: UnknownEmployee = {
+  name: 'Josh Mu',
+  privileges: ['tech'],
+}
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name)
+  if ('privileges' in emp) console.log('Privileges: ' + emp.privileges)
+  if ('startDate' in emp) console.log('StartDate: ' + emp.startDate)
+}
+
+printEmployeeInformation(em2)
+
+class Car {
+  drive() {
+    console.log('Driving..')
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving truck...')
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount)
+  }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive()
+  if (vehicle instanceof Truck) vehicle.loadCargo(10)
+}
+
+useVehicle(v1)
+useVehicle(v2)
