2d64a30d28c24fa89fc1098d3658ef3b5198cabb
Josh Mu <mu@joshmu.com>
2020-07-08 13:30:10 +1000

    more decorators

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,19 @@ function Logger(logTxt: string) {
   }
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId)
+    const p = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = p.name // '!' assumes we already get a return value
+    }
+  }
+}
+
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   // short hand syntax
   name = 'Josh'
