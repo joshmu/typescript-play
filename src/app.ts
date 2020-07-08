867727a162704c08a08b8782b0031db48348c398
Josh Mu <mu@joshmu.com>
2020-07-08 13:53:59 +1000

    different ways to use decorators

--- src/app.ts
+++ src/app.ts
@@ -38,3 +38,60 @@ class Person {
 const pers = new Person()
 
 console.log(pers)
+
+// ----
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property Decorator!')
+  console.log(target)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Property Accessor!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method Descriptor!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter Decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
+class Product {
+  @Log
+  title: string
+  private _price: number
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val
+    } else {
+      throw new Error('Invalid price - should be positive!')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this.price * (1 + tax)
+  }
+}
